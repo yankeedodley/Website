3e597eb50f83ce333fd311fb5aeb93a40a43d26b
yankeedodley <89747725+yankeedodley@users.noreply.github.com>
2023-10-07 10:17:40 -0700

    Added some more ideas

--- website.py
+++ website.py
@@ -7,16 +7,17 @@ st.title("Hi!")
 
 st.header("Welcome to the ideas generator!")
 
-if st.checkbox("The blog."):
-    st.write("Creative block is a common experience for people of all creative disciplines. It's the feeling of being stuck, unable to come up with new ideas or produce your best work.")
-    st.write('In this blog, we dicided to interview our friend Chunny, who recently experienced creative block.')
-    st.write('Us: "So Chunny, how did creative block feel like?"')
-    st.write('Chunny: "Creative block really sucks. Its like you cant think of anything. Your brain is basically dead."')
-    st.write('Us: "What made you get creative block?"')
-    st.write('Chunny: "Our art teacher told us to paint something, and I couldnt think of any good ideas."')
-    st.write('Us: "How did you overcome it?"')
-    st.write('Chunny: "I was lucky enough to have a friend who helped me. They made a website that helps people with creative block."')
-    st.write('"Its great!" He says "I can think again!"')
+if st.checkbox("Stuff we might delete"):
+    if st.checkbox("The blog."):
+        st.write("Creative block is a common experience for people of all creative disciplines. It's the feeling of being stuck, unable to come up with new ideas or produce your best work.")
+        st.write('In this blog, we dicided to interview our friend Chunny, who recently experienced creative block.')
+        st.write('Us: "So Chunny, how did creative block feel like?"')
+        st.write('Chunny: "Creative block really sucks. Its like you cant think of anything. Your brain is basically dead."')
+        st.write('Us: "What made you get creative block?"')
+        st.write('Chunny: "Our art teacher told us to paint something, and I couldnt think of any good ideas."')
+        st.write('Us: "How did you overcome it?"')
+        st.write('Chunny: "I was lucky enough to have a friend who helped me. They made a website that helps people with creative block."')
+        st.write('"Its great!" He says "I can think again!"')
 
 topic = None
 topics = None
@@ -29,17 +30,17 @@ if st.button("Submit"):
     
     st.subheader("Here's some ideas:")
     if topic == "Painting":
-        topics = ["Nature", "Self portrait", "Vehicles", "Thinking... Plz reload page.", "Stuff"]
+        topics = ["Nature", "Self portrait", "Vehicles", "Abstract", "Random Paint spashes"]
         for r in random_numbers:
             st.write(topics[r - 1])
 
     elif topic == "Music":
-        topics = ["Classical", "Fast and hard", "Slow and calming", "A", "B"]
+        topics = ["Classical", "Fast and hard", "Slow and calming", "Start playing on istrument, Write It down", "Stuff"]
         for r in random_numbers:
             st.write(topics[r - 1])
 
     elif topic == "Sculptures":
-        topics = ["Nature", "Humans", "Objects", "C", "D"]
+        topics = ["Nature", "Humans", "Objects", "start sqishing clay balls", "Thats kinda sus"]
         for r in random_numbers:
             st.write(topics[r - 1])
 
@@ -55,8 +56,3 @@ st.header("How to come up with your own ideas:")
 st.write("Step one: Think about what your latest peices are. What do you like?")
 st.write("Step two: Write the things down.")
 st.write("Step three: Make a picture related to the word.")
-
-
-
-if st.checkbox("Disclamer"):
-    st.write("None of the blog is real. We just made that up for this demo.")
